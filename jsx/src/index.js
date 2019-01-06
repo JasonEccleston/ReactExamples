245708f632476d2af886d0a95b614818818bb5d6
JsonScripts <jasoneccleston1@gmail.com>
2019-01-06 17:29:58 -0400

    test sample

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,8 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 //create a react component
+function getButtonText() {
+  return 'click on me!';
+}
+
 const App = () => {
-  return <div> Hi Hi! </div>;
+  const buttonText = { text: 'click me'};
+  return (
+  <div>
+    <label className="label" htmlFor="name">Enter Name: </label>
+    <input id="name" type="text"/>
+    <button style= {{ backgroundColor: 'blue', color: 'white' }}>{ buttonText.text }</button>
+  </div>
+  );
 };
 //take the react component and show on the browser
 
